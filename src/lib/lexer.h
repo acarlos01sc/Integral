ee28f068323d1764b92c61abd5130b419a81e7d7
Antonio Carlos <acarlos01.sc@gmail.com>
2026-01-22 12:55:38 -0300

    lexer.h

---
+++ src/lib/lexer.h
@@ -0,0 +1,28 @@
+// lexical reader
+
+#pragma once
+
+#include "token.h"
+#include <string>
+#include <vector>
+
+class Lexer {
+  public:
+    explicit Lexer(const std::string &input);
+
+    Token next();
+    std::vector<Token> tokenize();
+
+  private:
+    char peek() const;
+    char get();
+    void skip_white_space();
+
+    Token read_number();
+    Token read_identifier();
+
+    const std::string &input_;
+    size_t pos_;
+    int line_;
+    int column_;
+}
