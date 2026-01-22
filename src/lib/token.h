be97f0c53b2bb8a20293b8e5f5400a00c7b27b46
Antonio Carlos <acarlos01.sc@gmail.com>
2026-01-22 09:29:11 -0300

    lib

---
+++ src/lib/token.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+
+// Token type
+enum class TokenType {
+    Number,
+    Identifier,
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    LParen,
+    RParen,
+    EndOfFile,
+    Invalid
+};
+
+// Token
+struct Token {
+    TokenType type;
+    std::string lexeme;
+    std::size_t line;
+    std::size_t column;
+};
